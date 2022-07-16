876d21a1f1e86f0403bfe638a301f3f2a875e127
Gboyega Lawal <afiz.lawal51@gmail.com>
2022-07-16 15:56:48 +0100

    Added rider routes

--- src/routes/riderRoute.js
+++ src/routes/riderRoute.js
@@ -1,10 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { rideHistory, createBid, bidStatus } = require('../controllers/rider.controller');
+const {
+  rideHistory,
+  createBid,
+  bidStatus,
+  getRider,
+  viewRequest,
+  viewAllRequests,
+  AcceptRequest,
+  declineRequest,
+  riderEarnings,
+  endRide,
+} = require("../controllers/rider.controller");
 
-
-router.get('/history/:id', rideHistory);
-router.post('/bid', createBid);
-router.put('/status/:id', bidStatus);
+router.get("/riderEarnings", riderEarnings);
+router.get("/history/:id", rideHistory);
+router.post("/bid", createBid);
+router.put("/status/:id", bidStatus);
+router.get("/requests/:id", viewRequest);
+router.get("/requests", viewAllRequests);
+router.patch("/accept-request", AcceptRequest);
+router.patch("/decline-request", declineRequest);
+router.patch("/end-ride", endRide);
+router.get("/:id", getRider);
 
 module.exports = router;
