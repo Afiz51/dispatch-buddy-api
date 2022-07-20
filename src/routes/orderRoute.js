a47378ebe7c19d1c88a20fe65fb5f875517f02bd
Gboyega Lawal <afiz.lawal51@gmail.com>
2022-07-20 19:48:56 +0100

    Shippers orders endpoint

--- src/routes/orderRoute.js
+++ src/routes/orderRoute.js
@@ -1,12 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { orderStatus, createOrder, orderHistory, getOrderStatus } = require('../controllers/order.controller');
+const {
+  orderStatus,
+  createOrder,
+  orderHistory,
+  getOrderStatus,
+  getAllOrders,
+} = require("../controllers/order.controller");
 
-
-
-router.get('/orders/:id', orderHistory)
-router.post('/request', createOrder);
-router.put('/status/:id', orderStatus);
+router.get("/orders/:id", orderHistory);
+router.post("/request", createOrder);
+router.put("/status/:id", orderStatus);
 router.get("/order-status/:id", getOrderStatus);
+router.get("/shipper-orders/:id", getAllOrders);
 
 module.exports = router;
