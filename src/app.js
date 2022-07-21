2a9d00e5c516911c98596699ec8166f23ca76028
Gboyega Lawal <afiz.lawal51@gmail.com>
2022-07-21 08:46:27 +0100

    google map api

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ const connectDB = require("./db_config/db");
 const userAuthRouter = require("./routes/auth/user");
 const orderRoute = require("./routes/orderRoute");
 const riderRoute = require("./routes/riderRoute");
+const distanceMatrixRoute = require("./routes/distanceMatrixRoute");
 const transactionRoute = require("./routes/transactionRoute");
 
 const swaggerUI = require("swagger-ui-express");
@@ -39,6 +40,7 @@ app.use("/api/v1/auth", userAuthRouter);
 app.use("/api/v1/order", [orderRoute]);
 app.use("/api/v1/rider", riderRoute);
 app.use("/", transactionRoute);
+app.use("/api/v1/distance", distanceMatrixRoute);
 
 // middleware
 const notFoundMiddleware = require("./middleware/notFound");
